a8962730f120a2413d56097d5f64759c063c7eaa
kaderabdul <kaderabdul7980@gmail.com>
2022-04-27 22:51:06 +0600

    design the appbar

---
+++ src/components/DrawerBar.js
@@ -0,0 +1,16 @@
+// import { Drawer } from '@mui/material';
+// import React, { useState } from 'react';
+
+// const DrawerBar = () => {
+//     const [state, setState] = useState(false)
+//     return (
+//         <Drawer
+//         // anchor={anchor}
+//         open={state}
+//         // onClose={toggleDrawer(anchor, false)}
+//         >
+//         </Drawer>
+//     );
+// };
+
+// export default DrawerBar;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,52 @@
-import { AppBar, Toolbar } from '@mui/material';
-import {Menu} from '@mui/icons-material';
+import { AppBar, Button, Collapse, Drawer, List, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material';
+import {Drafts, ExpandLess, ExpandMore, Inbox, Menu, Send, StarBorder} from '@mui/icons-material';
 import Typography from '@mui/material/Typography';
+import styled from '@emotion/styled';
+import { Box } from '@mui/system';
+import DrawerBar from './DrawerBar';
+import { useState } from 'react';
+
+const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent:"space-between"
+})
 
 const Navbar = () => {
-    return (
-        <AppBar position="sticky">
-            <Toolbar variant="dense">
-                <Menu />
-                <Typography variant='span'>hello</Typography>
+    const [openDrawer, setOpenDrawer] = useState(false)
+    const toggleDrawer = (e) => {
+        // console.log(openDrawer);
+        // console.log(e);
+        setOpenDrawer(e);
+    }; 
+    
+    // Nested List Item
+    const [openNestedList, setOpenNestedList] = useState(true);
 
-            </Toolbar>
+    const handleNestedList = () => {
+        setOpenNestedList(!openNestedList);
+    };
+
+    return (
+        <AppBar position="sticky" sx={{bgcolor: 'background.paper',color:'black',boxShadow: 0}}>
+            <StyledToolbar>
+                <Typography variant='h6' marginRight='5px'>JobLab.</Typography>
+
+                <Box sx={{ display: {xs:"none",sm:"block"} }}>
+                    <Box sx={{display: 'flex',gap:5,alignItems:'center'}}>
+                    <Typography variant='span'>Home</Typography>
+                    <Typography variant='span'>About</Typography>
+                    <Typography variant='span'>Job</Typography>
+                    <Typography variant='span'>Blog</Typography>
+                    <Typography variant='span'>Contact</Typography>
+
+                    <Button disableElevation variant="contained">Find Job</Button>
+                    <Menu  onClick={()=>toggleDrawer(true)} />
+                    </Box>
+                </Box>
+                {/* <Menu onClick={()=>toggleDrawer(true)} sx={{ display: {xs:"block",sm:"none"} }} /> */}
+
+
+            </StyledToolbar>
         </AppBar>
     );
 };
