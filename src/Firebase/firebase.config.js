124a13f25b552fe43dd51ef539343add4bb8fbb6
kaderabdul <kaderabdul7980@gmail.com>
2022-05-25 15:23:44 +0600

    add description,features about this project

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,12 +1,4 @@
-console.log(process.env);
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_APP_ID,
-// };
+// console.log(process.env);
 const firebaseConfig = {
     apiKey: "AIzaSyB_5v48e3ckQjXazkoYfI-sBEkxZIX2Obs",
     authDomain: "jobslist-7d4bc.firebaseapp.com",
